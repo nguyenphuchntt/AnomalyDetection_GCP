76a0c9bfc04f74df253b988724a291a1a29fa08b
tuan3705 <dtuanat3705@gmail.com>
2025-11-16 21:58:20 +0700

    data to prediction_data

--- prediction_data/main.py
+++ prediction_data/main.py
@@ -1,19 +1,15 @@
-import os
 import base64
 import json
 import functions_framework
 from google.cloud import bigquery 
 
 BIGQUERY_DATASET = 'fraud_dashboard_data'
-BIGQUERY_TABLE = 'history_db'
+BIGQUERY_TABLE = 'prediction_data'
 
 bigquery_client = None
 
-def get_actual_result(failure_flag):
-    if failure_flag == 0:
-        return 0
-    else:
-        return None
+def get_checked_status(failure_flag):
+    return True if failure_flag == 0 else False
 
 @functions_framework.cloud_event
 def main_handler(cloud_event):
@@ -35,22 +31,15 @@ def main_handler(cloud_event):
         return 
 
     try:
-        transaction_id_value = data["id"]
-        prediction_result_value = data["failure"]
-        failure_flag = data["failure"]
-        prediction_score_value = data["prediction_score"]
-        amount_value = data["amount"]
-        time_value = data["time"]
-        
-        actual_result_value = get_actual_result(failure_flag)
+        transaction_id = data["id"]
+        prediction_result = data["failure"]
+
+        checked_status = get_checked_status(prediction_result)
         
         rows_to_insert = [{
-            "transaction_id": transaction_id_value,
-            "prediction_score": prediction_score_value,
-            "prediction_result": prediction_result_value,
-            "actual_result": actual_result_value,
-            "amount": amount_value,
-            "time": time_value
+            "transaction_id": transaction_id,
+            "prediction_result": prediction_result,
+            "checked": checked_status
         }]
         
         table_ref = bigquery_client.dataset(BIGQUERY_DATASET).table(BIGQUERY_TABLE)
@@ -58,10 +47,9 @@ def main_handler(cloud_event):
         errors = bigquery_client.insert_rows_json(table_ref, rows_to_insert)
 
         if errors:
-            print("Errors encountered while inserting rows:")
-            print(errors)
+            print("Errors encountered while inserting rows:", errors)
         else:
-            print(f"Successfully inserted record for Transaction ID: {data['id']}.")
+            print(f"Successfully inserted record for ID: {transaction_id} into prediction_data.")
             
     except Exception as e:
         print(f"An error occurred during BigQuery insertion: {e}")
